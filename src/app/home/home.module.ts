825741d606052c9ee474891dc7c3619d9208f53e
Danilo C. Graciano <danilocgraciano@gmail.com>
2018-04-06 23:53:28 -0300

    Inclusão de tabela com paginação.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,18 +5,22 @@ import { NavbarComponent } from "./navbar/navbar.component";
 import { AuthModule } from "../auth/auth.module";
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SidebarItemComponent } from './sidebar-item/sidebar-item.component';
+import { TableComponent } from './table/table.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   imports: [
     CommonModule,
-    AuthModule
+    AuthModule,
+    NgxPaginationModule
   ],
   exports: [HomeComponent],
   declarations: [
     HomeComponent,
     NavbarComponent,
     SidebarComponent,
-    SidebarItemComponent
+    SidebarItemComponent,
+    TableComponent
   ]
 })
 export class HomeModule { }
---
+++ src/app/home/table/table.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+
+  header: Array<string> = ['ID', 'Usuário', 'E-mail'];
+
+  data: Array<any> = [
+    { id: 2, usuario: 'Danilo', email: 'danilo@compels.net' },
+    { id: 3, usuario: 'Suporte', email: 'suporte@compels.net' },
+    { id: 4, usuario: 'Alex', email: 'alex@compels.net' },
+    { id: 5, usuario: 'Wesley', email: 'wesley@compels.net' },
+    { id: 6, usuario: 'Leonardo', email: 'leonardo@compels.net' },
+    { id: 7, usuario: 'Jader', email: 'jader@compels.net' },
+    { id: 8, usuario: 'Renann', email: 'renann@compels.net' },
+    { id: 9, usuario: 'Fernando', email: 'fernando@compels.net' },
+    { id: 10, usuario: 'Lucas', email: 'lucas@compels.net' },
+    { id: 11, usuario: 'Alessandro', email: 'alessandro@compels.net' },
+    { id: 12, usuario: 'José Roberto', email: 'j.roberto@compels.net' },
+    { id: 13, usuario: 'Denilson', email: 'denilson@compels.net' },
+    { id: 14, usuario: 'Éliphas', email: 'eliphas@compels.net' },
+    { id: 15, usuario: 'Pedro', email: 'pedro@compels.net' },
+    { id: 16, usuario: 'Vander', email: 'vander@compels.net' }
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+
+}
