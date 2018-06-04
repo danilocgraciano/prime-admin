bc3514027503b4d7cfda86a68984da2fdbeafbd5
Danilo C. Graciano <danilo@compels.net>
2018-06-04 17:23:26 -0300

    Correção.

--- src/app/+sca/sca.module.ts
+++ src/app/+sca/sca.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScaRoutingModule } from './sca.routing.module';
-import { ClasseService } from './menu/classe.service';
 
 @NgModule({
   imports: [
